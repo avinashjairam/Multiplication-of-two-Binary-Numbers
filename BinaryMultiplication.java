14bc8124446a98c6f9abdb0202a2c004626c2b17
Avinash <avinash.jairam@gmail.com>
2016-12-07 16:35:19 -0500

    New Design needed. e.g. a user interface which accepts decimal values and convert them to binary using the conventional method and divide and conquer

--- BinaryMultiplication.java
+++ BinaryMultiplication.java
@@ -27,11 +27,140 @@ public class BinaryMultiplication{
 	int [] num1;
 	int [] num2;
 
+	int count1 =0;
+	int count2=0;
 	
 
 	String num1AsString, num2AsString;
+
+	public static void main(String [] args){
+		
+	//	int [] num1 = new int [] {1,0,1,1,0,1};
+	//	int [] num2 = new int [] {1,1,0,1};
 	
+//		int [] num1 = new int [] {1,1,1,1};
+//		int [] num2 = new int [] {1,1,1,1};
+
 	
+		int [] num1 = new int [] {1,1};
+		int [] num2 = new int [] {1,1};
+
+	//	totalBits = num1.length * num2.length;
+
+		BinaryMultiplication obj = new BinaryMultiplication(num1,num2);
+		obj.multiply();
+		obj.addition();
+		System.out.println("regular mulitiplication #steps " + obj.count1);
+/*
+		ArrayList<Integer>number1 = new ArrayList<Integer>();
+		ArrayList<Integer>number2 = new ArrayList<Integer>();
+
+		for(int i = 0; i <num1.length; i++){
+			number1.add(num1[i]);
+		}
+
+		for(int j=0; j <num2.length; j++){
+			number2.add(num2[j]);
+		}
+
+//		System.out.println("Divide and Conquer result: ");
+		System.out.println("Divide and Conquer result: " + Integer.toBinaryString(obj.divideAndConquer(number1,number2)));
+		System.out.println("divide and conquer #steps: " + obj.count2); */
+
+	}
+
+	public int divideAndConquer(ArrayList<Integer>num1, ArrayList<Integer>num2){
+		count2++;
+		 makeEqual(num1,num2);
+
+		int size = num1.size();
+		if(size == 0){
+			return 0;
+		}
+
+		System.out.println (num1 + " " + num2);
+		if(size == 1){
+			System.out.println(" base case multiplication " + num1.get(0) * num2.get(0));
+			return num1.get(0) * num2.get(0);
+
+		}
+
+		int firstPartOfNumber = size/2;
+		int secondPartOfNumber = size - firstPartOfNumber;
+
+		ArrayList<Integer> Xh = new ArrayList<Integer>();
+		ArrayList<Integer> Xl = new ArrayList<Integer>();
+		ArrayList<Integer> Yh = new ArrayList<Integer>();
+		ArrayList<Integer> Yl = new ArrayList<Integer>();
+
+	
+
+		for(int i =0; i < firstPartOfNumber; i++){
+			Xh.add(num1.get(i));
+			Yh.add(num2.get(i));
+		}
+
+		for(int i =secondPartOfNumber; i < size; i++){
+			Xl.add(num1.get(i));
+			Yl.add(num2.get(i));
+		}
+
+		System.out.println(Xh);
+
+		int a = divideAndConquer(Xh,Yh);
+		int d = divideAndConquer(Xl,Yl);
+
+		ArrayList<Integer> localSum1 = new ArrayList<Integer>();
+		addTwoBinaryNumbers(Xh,Xl);
+		copyArrayList(sum,localSum1);
+
+		ArrayList<Integer> localSum2 = new ArrayList<Integer>();
+		addTwoBinaryNumbers(Yh,Yl);
+		copyArrayList(sum,localSum2);
+
+		System.out.println("local sum1 = " + localSum1 + " local sum2 = " + localSum2);
+	
+		int e = divideAndConquer(localSum1,localSum2);
+
+	
+//		int e=0;
+		System.out.println("a = " + a + " d = " + d + " e = " + e);
+//		System.out.println("steps " + count1);
+	 	
+	
+		return  a *(1<<(2*secondPartOfNumber)) +(e - a - d) *  (1 <<secondPartOfNumber) + d;
+
+		//return 0;
+
+	}
+
+	public void copyArrayList(ArrayList<Integer>source, ArrayList<Integer>destination){
+
+		for(int i=0;i<source.size();i++){
+			destination.add(source.get(i));
+		}
+	}
+
+	public void makeEqual(ArrayList<Integer>num1, ArrayList<Integer>num2){
+		
+
+		if(num1.size() == num2.size()){
+			return;
+		}
+		else if(num1.size() > num2.size() ){
+			for(int i = num2.size(); i <num1.size(); i++){
+				num2.add(0, 0);
+			}
+		}
+				
+		else{
+			for(int i = num1.size(); i <num2.size(); i++){
+				num1.add(0, 0);
+			}		
+		}
+	}
+
+
 	public BinaryMultiplication(int [] num1, int [] num2){
 
 
@@ -47,19 +176,11 @@ public class BinaryMultiplication{
 	}
 
 
-	public static void main(String [] args){
-		
-		int [] num1 = new int [] {1,0,1,1,0,1};
-		int [] num2 = new int [] {1,1,0,1};
-		
-	//	totalBits = num1.length * num2.length;
-
-		BinaryMultiplication obj = new BinaryMultiplication(num1,num2);
-		obj.multiply();
-		obj.addition();
-	}	
+	
 
 	public String addTwoBits(int x, int y){
+		count1++;
+		count2++;
 		String returnValue="";
 		System.out.println("x is " + x + " y is " +y);	
 		if(x == 0 && y == 0){
@@ -80,7 +201,7 @@ public class BinaryMultiplication{
 		else if(x == 10 && y == 1){
 			returnValue = "11";
 		}
-		System.out.println("return value is " + returnValue);
+//		System.out.println("return value is " + returnValue);
 
 		return returnValue;
 	}
@@ -93,43 +214,45 @@ public class BinaryMultiplication{
 //		int [] sum;
 
 		 addTwoBinaryNumbers(listOfLists.get(0),listOfLists.get(1));
-	
+		count1++;
+		count1++;
 	//	ArrayList<Integer> partialSum = Arrays.asList(sum); 
 		for(int i =2; i < listOfLists.size(); i++){
 			addTwoBinaryNumbers(listOfLists.get(i),sum);
-			
+			count1++;
 		}
 	}
 
 	public void addTwoBinaryNumbers(ArrayList<Integer>num1, ArrayList<Integer>num2){
+		count1++;
 		sum=null;
 		 sum = new ArrayList<Integer>();
 		String result="";
 		ArrayList<Integer>carry = new ArrayList<Integer>();
 
-		System.out.println("\nNow working on " + num1 + " " + num2);
-		System.out.println();
+//		System.out.println("\nNow working on " + num1 + " " + num2);
+//		System.out.println();
 	
 		for(int i =num1.size()-1; i>=0; i--){
 			result=addTwoBits(num1.get(i),num2.get(i));
-			System.out.println("Initial Result is " + result);
+//			System.out.println("Initial Result is " + result);
 			
 			if(carry.size() != 0){
-				System.out.println("Carry loaded: " + carry.get(carry.size()-1) + " result is " + result);
+//				System.out.println("Carry loaded: " + carry.get(carry.size()-1) + " result is " + result);
 				result=addTwoBits(Integer.parseInt(result),carry.get(carry.size()-1));
-				System.out.println("result after carry " + result);
+//				System.out.println("result after carry " + result);
 				carry.remove(carry.size()-1);
 			
 
 			}
 
-			System.out.println("i = " + i + " result is " + result + " carry is " + carry + " result length is " + result.length());
+//			System.out.println("i = " + i + " result is " + result + " carry is " + carry + " result length is " + result.length());
 
 
 			if(result.length() == 2){
 				carry.add(Integer.parseInt(result.substring(0,1)));
 				result = Character.toString(result.charAt(1));
-				System.out.println(carry);			
+//				System.out.println(carry);			
 				sum.add(Integer.parseInt(result));
 				
 			}
@@ -148,8 +271,8 @@ public class BinaryMultiplication{
 
 		}
 		Collections.reverse(sum);	
-		System.out.print("sum is " +sum + " carry is " + carry);	
-		
+//		System.out.print("sum is " +sum + " carry is " + carry);	
+		System.out.println("sum is " + sum);		
 		/*int [] sum1 =  new int [sum.size()];
 		sum1 = sum.toArray(sum1);
 
@@ -174,18 +297,19 @@ public class BinaryMultiplication{
 			
 			for(int j=num2.length-1; j >= 0; j--){
 				additionNumbers.add(num1[i]*num2[j]);
-			//	System.out.print(num1[i]  * num2[j] + " ");
+				count1++;	
+		//	System.out.print(num1[i]  * num2[j] + " ");
 			}
 //		
-			System.out.println(additionNumbers);
+//			System.out.println(additionNumbers);
 			paddedZeros(additionNumbers,num1.length,num2.length);
 			Collections.reverse(additionNumbers);
 			this.listOfLists.add(additionNumbers);
-			System.out.println(additionNumbers);
-			System.out.println();
+//			System.out.println(additionNumbers);
+//			System.out.println();
 			//System.out.println(listOfLists);
 		}
-		System.out.println("list of lists " + listOfLists);
+//		System.out.println("list of lists " + listOfLists);
 
 	
 	}
